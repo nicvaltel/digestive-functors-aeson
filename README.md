225bc39d965f15cd8909d94460ef6b91ec287d03
nicvaltel <nicvaltel@gmail.com>
2024-07-22 12:57:02 +0300

    Fork. Updated lens and lens-aeson versions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # digestive-functors-aeson
-
+Fork of http://github.com/ocharles/digestive-functors-aeson 
+Updated lens and lens-aeson versions
 Run `digestive-functors` forms against JSON documents (via `aeson`).
--- src/Text/Digestive/Aeson.hs
+++ src/Text/Digestive/Aeson.hs
@@ -12,7 +12,6 @@ import Control.Monad (join)
 import Data.Aeson (ToJSON(toJSON), Value(..), object)
 import Data.Aeson.Lens
 import Data.Maybe (fromMaybe)
-import Data.Monoid (mempty)
 import Safe (readMay)
 import Text.Digestive
 import Text.Digestive.Form.List (unparseIndices)
@@ -20,6 +19,7 @@ import Text.Digestive.Form.List (unparseIndices)
 import qualified Data.IntMap.Strict as IntMap
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import Unsafe.Coerce(unsafeCoerce)
 
 --------------------------------------------------------------------------------
 {-| Given a JSON document and a form, attempt to use the JSON document to
@@ -85,7 +85,7 @@ jsonErrors v =
 pathToLens :: [T.Text] -> Traversal' (Maybe Value) (Maybe Value)
 pathToLens = foldl (.) id . map pathElem . filter (not . T.null)
   where
-    pathElem p = maybe (non (object []) . _Object . at p)
+    pathElem p = maybe (non (object []) . _Object . at (unsafeCoerce p))
                        (\n -> non (Array mempty) . _Array . iso toMap fromMap . at n)
                        (readMay $ T.unpack p)
     toMap = V.ifoldl' (\m i a -> IntMap.insert i a m) IntMap.empty
